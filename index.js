1b3c284165c43f6a98279a2739cde1ee651e3460
webnur <mdnuruli291@gmai.com>
2022-11-25 07:02:38 +0600

    mongodb added and prodcuts and product category method added

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port ||5000
+const port = process.env.port || 5000
+require('dotenv').config();
 const app = express();
 
+
+//middleware
 app.use(cors());
 app.use(express.json())
 
+
+//dbuser = salesBd
+//password = Ago3oXoqLFul9xkZ
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6x8xxck.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run () {
+    const productsCollection = client.db('salesBb').collection('products');
+    const CategoriesCollection = client.db('salesBb').collection('categories');
+
+    app.get('/products', async(req, res) => {
+        const query = {};
+        const products = await productsCollection.find(query).toArray();
+        res.send(products)
+    })
+    app.get('/categories', async(req, res) => {
+        const query = {};
+        const categories = await CategoriesCollection.find(query).toArray();
+        res.send(categories)
+    })
+    app.get('/category/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {category_id: id}
+        const category = await productsCollection.find(query).toArray()
+        res.send(category)
+    })
+
+}
+
+run().catch(error => console.error(error))
 app.get('/', (req, res) => {
     res.send('Assignment 12 Server is run')
 });

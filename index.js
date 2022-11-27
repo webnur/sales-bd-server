39298e56c877656ef84536e1f31d10c05ef3d688
webnur <mdnuruli291@gmai.com>
2022-11-27 10:06:16 +0600

    json web token added and verified alhamdulillah

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res, next){
+    console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorized access')
+    }
+    const token =authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_TOKEN, function(err, decoded){
+        if(err){
+            return res.status.send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 async function run() {
     try {
@@ -60,14 +76,24 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/bookings', async (req, res) => {
-            let query = {};
+        app.get('/bookings',verifyJWT, async (req, res) => {
+            // let query = {};
             const email = req.query.email;
-            if (email) {
-                query = {
-                    email: email,
-                }
+
+            // if (email) {
+            //     query = {
+            //         email: email,
+            //     }
+            // }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
             }
+
+
+            const query = {email: email}
+
             const booking = await bookingCollection.find(query).toArray();
             res.send(booking)
         })
